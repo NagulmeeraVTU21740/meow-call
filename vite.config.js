45b67fc6c417823e53970394fc63a88e3cd6cc99
NagulmeeraVTU21740 <vtu21740@veltech.edu.in>
2025-04-13 12:15:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/meow-call/', // This is CRUCIAL for GitHub Pages
+  base: '/meow-call/', // MUST match your repo name
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
